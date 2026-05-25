8a17b1bfc2a1cb92cd61a6c3069a2cb98c6741b5
Anvarjon <201946044@hs.ac.kr>
2026-05-25 20:27:45 +0900

    feat: develop avtive users page screen components

--- src/pages/homepage/ActiveUsers.tsx
+++ src/pages/homepage/ActiveUsers.tsx
@@ -1,5 +1,28 @@
+import '../../css/home/ActiveUsers.css';
+
 function ActiveUsers() {
-	return <div className="text-4xl font-bold">Active Users</div>;
+	const users = [
+		{ id: 1, name: 'Ali', image: '/images/banner1.jpg' },
+		{ id: 2, name: 'Sara', image: '/images/banner2.jpg' },
+		{ id: 3, name: 'John', image: '/images/banner4.jpg' },
+		{ id: 4, name: 'Emma', image: '/images/banner5.jpg' },
+		{ id: 5, name: 'David', image: '/images/banner3.jpg' },
+	];
+
+	return (
+		<div className="active-users">
+			<h2 className="title">Active Users</h2>
+
+			<div className="users-grid">
+				{users.map((u) => (
+					<div key={u.id} className="user-card">
+						<img src={u.image} alt={u.name} />
+						<p>{u.name}</p>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default ActiveUsers;
